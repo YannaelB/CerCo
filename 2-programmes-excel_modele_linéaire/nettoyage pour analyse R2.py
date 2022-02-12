822fa32f55416cb8b3386893a36ce3c3351a720e
YannaelB <71601928+YannaelB@users.noreply.github.com>
2022-02-12 14:36:33 +0100

    Add files via upload

---
+++ 2-programmes-excel_modele_linéaire/nettoyage pour analyse R2.py
@@ -0,0 +1,174 @@
+import numpy as np
+from openpyxl import load_workbook
+from openpyxl import Workbook
+import matplotlib.pyplot as plt
+import os
+from openpyxl.styles import Color,PatternFill
+from numpy.linalg import solve
+from collections import Counter
+import statistics as st
+from scipy import stats
+
+
+#Regarder le programme "nettoyage pour analyse R" pour avoir plus de commentaires
+
+def ouvrirXLSX(filepath) :
+    wb = load_workbook(filepath)
+    return wb,wb.sheetnames
+
+def lireFeuille(wb,nom_feuille,prem_ligne = 1,der_ligne=np.inf):
+    ws = wb[nom_feuille]
+    tableau = []
+    der_ligne = min(der_ligne,ws.max_row)
+    for i,row in enumerate(ws.rows,1) : # pour chaque ligne,
+        if prem_ligne <= i <= der_ligne :
+            ligne = []
+            for cell in row : # pour chaque cellule de la ligne,
+                ligne.append(cell.value) # lecture de son contenu
+            tableau.append(ligne)
+    tableau = np.array(tableau) # Conversion en tableau numpy
+    shp,typ_don = tableau.shape,tableau.dtype
+    return tableau
+
+
+
+classeur = "Sujet_N_MutilAnalysis-nettoyé3.1.xlsx" #chemin du fichier créé par "nettoyage excel_v3"
+
+
+listeFeuilles = [lireFeuille(ouvrirXLSX(classeur)[0],i) for i in (ouvrirXLSX(classeur)[1])]
+
+
+def etude_stimuli(L,s,R,n):
+    nb_hand = []
+    nb_head = []
+    TR = []
+    Errh = []
+    Errv = []
+    Errp = []
+    Err3D = []
+    ErrHead = []
+    for k in range(1,136):
+        if int(L[s][k,2]) == R:
+            if int(L[s][k,3]) == n: 
+                if np.isnan(float(L[s][k,8])) == False:
+                    TR.append(float(float(L[s][k,7])))
+                    nb_hand.append(float(float(L[s][k,6])))
+                    nb_head.append(float(float(L[s][k,5])))
+                    Errh.append(abs(float(L[s][k,9])))
+                    Errv.append(abs(float(L[s][k,10])))
+                    Errp.append(abs(float(L[s][k,11])))
+                    Err3D.append(abs(float(L[s][k,8])))
+                if np.isnan(float(L[s][k,12])) == False:
+                    ErrHead.append(abs(float(L[s][k,12])))
+
+    return nb_head,nb_hand,TR,Err3D,Errh,Errv,Errp,ErrHead
+
+def in_cong(L,s,R,n1,n2):
+    nb_hand1 = []
+    nb_head1 = []
+    TR1 = []
+    Errh1 = []
+    Errv1 = []
+    Errp1 = []
+    Err3D1 = []
+    ErrHead1 = []
+    for i in range(n1,n2+1):
+        nb_head,nb_hand,TR,Err3D,Errh,Errv,Errp,ErrHead = etude_stimuli(L,s,R,i)
+        for j in range(len(Errh)):
+            nb_head1.append(nb_head[j])
+            nb_hand1.append(nb_hand[j])
+            TR1.append(TR[j])
+            Errh1.append(Errh[j])
+            Errv1.append(Errv[j])
+            Errp1.append(Errp[j])
+            Err3D1.append(Err3D[j])
+        for k in range(len(ErrHead)):
+            ErrHead1.append(ErrHead[k])
+    return st.mean(nb_head1),st.mean(nb_hand1),st.mean(TR1),st.mean(Err3D1),st.mean(Errh1),st.mean(Errv1),st.mean(Errp1),st.mean(ErrHead1)
+            
+
+    
+
+def création_tab_R(L):
+    n=len(L)
+    print("n : ",n)
+    T = np.zeros([6*n+1,11])
+    #T[0,:]=["Subject","Room","Cong"," nb_head_move","HAND_numberOfMovement","HEAD_RT","Distance_meanSound_3D","HandSignedError_h","HandSignedError_v","HandDepth_signed"]
+    w = 1
+    for i in range(n):
+        T[w:w+9,0] = i+1
+        for k in range(1,3): #pour la scène plage/église
+            for l in range(1,4): #parcours les sons congruents/incongruents et neutres
+                if k == 1:
+                    if l == 1:
+                        a,b,c,d,e,f,g,h=in_cong(L,i,8,1,9) #congruents #retourne la moyenne des sons 1 à 9 dans la plage (8) pour le sujet i
+                        T[w,1:11] = k,l,a,b,c,d,e,f,g,h
+                    elif l == 2:
+                        a,b,c,d,e,f,g,h=in_cong(L,i,8,10,18) #incongruents
+                        T[w,1:11] = k,l,a,b,c,d,e,f,g,h
+                    elif l == 3:
+                        a,b,c,d,e,f,g,h=in_cong(L,i,8,19,21) #neutres
+                        T[w,1:11] = k,l,a,b,c,d,e,f,g,h
+                elif k == 2:
+                    if l == 1:
+                        a,b,c,d,e,f,g,h=in_cong(L,i,10,10,18) #congruents
+                        T[w,1:11] = k,l,a,b,c,d,e,f,g,h
+                    elif l == 2:
+                        a,b,c,d,e,f,g,h=in_cong(L,i,10,1,9)
+                        T[w,1:11] = k,l,a,b,c,d,e,f,g,h
+                    elif l == 3:
+                        a,b,c,d,e,f,g,h=in_cong(L,i,10,19,21)
+                        T[w,1:11] = k,l,a,b,c,d,e,f,g,h
+                w = w+1
+                
+    return T
+
+
+
+def creationwb(L):
+    wb=Workbook()
+
+    ws1 = wb.create_sheet("sujet_")
+        
+    # on rempli le tableau cellule par cellule
+        
+    for ligne in range (1,L.shape[0]+1) :
+        for colonne in range (1,L.shape[1]+1):
+            #ws1.cell(row = nblignes+1 , column = colonne)
+            ws1.cell(row= ligne, column = colonne ,value = L[ligne-1][colonne-1] )
+    ws1.cell(1,1 ,value = "Subject" )
+    ws1.cell(1,2 ,value = "Room" )
+    ws1.cell(1,3 ,value = "Cong" )
+    ws1.cell(1,4 ,value = "nb_head_move" )
+    ws1.cell(1,5 ,value = "HAND_numberOfMovement" )
+    ws1.cell(1,6 ,value = "HEAD_RT" )
+    ws1.cell(1,7 ,value = "Distance_meanSound_3D" )
+    ws1.cell(1,8 ,value = "HandSignedError_h" )
+    ws1.cell(1,9 ,value = "HandSignedError_v" )
+    ws1.cell(1,10 ,value = "HandDepth_signed" )
+    ws1.cell(1,11 ,value = "Head_error_orientation" )
+                
+
+                
+                
+   
+    del wb["Sheet"]
+    wb.save("Excel_pour_analyze_R2.xlsx")
+    return print("l'excel à été créé ;) ")
+
+#creationwb(M)
+                
+                
+T = création_tab_R(listeFeuilles)
+creationwb(T)
+        
+
+        
+        
+
+
+
+
+
+
+        
